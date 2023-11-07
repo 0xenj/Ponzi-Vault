fcc5a7de3cc733851e51f081cd5a255c92762a57
enzo jakobasch <enzo.jakobasch@gmail.com>
2023-11-07 15:12:35 +0100

    improve contract

--- contracts/ponziVault.sol
+++ contracts/ponziVault.sol
@@ -7,13 +7,36 @@ import './abstracts/Controlable.sol';
 contract Vault is Controlable {
   IERC20 public immutable token;
 
+  uint public totalSupply;
+  uint public finishAt;
+  uint public rewardRate;
+  uint public rewardPerTokenStaked;
+  uint public constant THIRTY_DAYS = 30 days;
+  uint public constant NINETY_DAYS = 90 days;
+  uint public constant HUNDRED_EIGHTY_DAYS = 180 days;
+
   mapping(address => bool) private supportedToken;
   mapping(address => uint) public balanceOf;
-  uint public totalSupply;
 
   constructor(address _token) {
     token = IERC20(_token);
   }
 
-  function stacking() external {}
+  function staking(uint _amount) external {}
+
+  function claimReward() external {}
+
+  function _min(uint x, uint y) private pure returns (uint) {
+    return x <= y ? x : y;
+  }
+
+  function durationLeft() external view returns (uint) {}
+
+  function updateRewardRate(uint _rewardRate) external {}
+
+  function getRewardRate() public view returns (uint) {}
+
+  function updateRewardPerTokenStaked(uint _RewardPerToken) external {}
+
+  function getRewardPerToken() public view returns (uint) {}
 }
