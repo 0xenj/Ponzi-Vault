503b03880c77e0a0f4c6b5c9686e308248d37d6e
enzo jakobasch <enzo.jakobasch@gmail.com>
2023-11-07 14:38:51 +0100

    improve contract vault

--- contracts/ponziVault.sol
+++ contracts/ponziVault.sol
@@ -1,4 +1,19 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.19;
 
-contract Vault {}
+import '@openzeppelin/contracts/token/ERC20/IERC20.sol';
+import './abstracts/Controlable.sol';
+
+contract Vault is Controlable {
+  IERC20 public immutable token;
+
+  mapping(address => bool) private supportedToken;
+  mapping(address => uint) public balanceOf;
+  uint public totalSupply;
+
+  constructor(address _token) {
+    token = IERC20(_token);
+  }
+
+  function stacking() external {}
+}
